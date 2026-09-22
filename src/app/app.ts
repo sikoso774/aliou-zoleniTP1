12283bd9c2196bb083649d902d5d5dc7207079a8
Zoléni KZ <zolenikokolozassi@gmail.com>
2026-09-22 16:30:10 +0200

    question 5

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,9 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgClass } from '../../node_modules/@angular/common/types/_common_module-chunk';
 
 @Component({
-  imports: [RouterOutlet],
+  // imports: [RouterOutlet, NgClass],
   selector: 'app-root',
   styleUrl: './app.scss',
   templateUrl: './app.html',
